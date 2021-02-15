19f05732bf1d78ff4e5ffd642c7d646b5ee10148
Juan P <code@qjuanp.dev>
2021-02-15 13:58:17 +0000

    Functions declaration

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of spades" # Full declaration
-	card := "Ace of spades"   // Short declaration and assignation with type inferance
-	card = "Five of Diamonds" // Assignation
+	card := newCard()
 
 	fmt.Println(card)
+	fmt.Println(printState())
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
