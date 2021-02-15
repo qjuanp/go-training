b100b120a3b88261858fbe8748c31cd300d3beb7
Juan P <code@qjuanp.dev>
2021-02-15 14:45:26 +0000

    Exercise
    New Deck declaration

--- cards/main.go
+++ cards/main.go
@@ -1,33 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of spades" # Full declaration
-	card := newCard()
+	cards := newDeck()
 
-	fmt.Println(card)
-	fmt.Println(printState())
-
-	// Array -> fixed lenght
-	// Slice -> An array that can growth
-	cards := deck{"Five of Diadmonds", "Ace of Spades", newCard(), newCard()}
-	cards = append(cards, "Six of Spades") // creates a new Array (Inmutability)
-
-	fmt.Println(cards)
 	cards.print()
-
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
-}
-
-type laptopSize float
-
-func (this laptopSize) get() {
-	return this
 }
--- cards/state.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func printState() string {
-	return "Dublin"
-}
