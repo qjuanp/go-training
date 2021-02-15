203933aea3be7185f1e21840d74c3c18b58b85e4
Juan P <code@qjuanp.dev>
2021-02-15 16:06:38 +0000

    Read from file
    Cast []byte -> string -> []string -> deck

--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	// var card string = "Ace of spades" # Full declaration
 	cards := newDeck()
 	cards.saveToFile("my_cards")
+
+	newCards := newDeckFromFile("my_cards")
+	fmt.Println("---- New Deck from file")
+	newCards.print()
 }
