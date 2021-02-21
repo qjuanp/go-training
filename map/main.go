4e8db49051822d0d137b895754fac2d7ebfd26e3
Juan P <code@qjuanp.dev>
2021-02-21 21:29:53 +0000

    Iteration over maps

--- map/main.go
+++ map/main.go
@@ -9,10 +9,16 @@ func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4b5869",
+		"white": "#ffffff",
+		"black": "#000000",
 	}
 
-	colors["white"] = "#000000"
-
-	delete(colors, "white")
 	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Printf("The color %s is represented by %s\n", color, hex)
+	}
 }
